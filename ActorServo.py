be6c0c47475a68f79ce4c3141345d3704e5ce4d2
Todd Kleman <toddkleman@gmail.com>
2023-03-07 16:47:36 -0600

    Update ActorServo.py

--- ActorServo.py
+++ ActorServo.py
@@ -20,37 +20,7 @@ sneezy = servo.Servo(pca.channels[6])# 80 down, 100 up, 2
 bashful = servo.Servo(pca.channels[7])# 70down, 90up, 2
 snow = servo.Servo(pca.channels[8]) # 60, 120, 1
 '''
-def moveServo(start,stop,delta):
-    incMove = (stop-start)/100.0
-    incTime = delta/100.0
-    #using start angle(first value in moveServo) plus incremental moves(incMove) to rotate servo to stop angle in time(delta) increments(incTime) specified
-    for x in range(100):
-        witch.angle = start + x*incMove 
-        #doc.angle = stop+10 - x*(incMove + .3) # 0.3 to account for incTime and incMove to keep servo in line with start/stop values,
-                                               # otherwise servo will jump at endpoints. stop used in this line to rotate servo opposite direction
-        #happy.angle = start+15 + x*(incMove - .35)
-        #dopey.angle = start+20 + x*(incMove - .35)
-       # sleepy.angle = start+5 + x*(incMove - .35)
-        grumpy.throttle = -.125 # minus is a forward rotation, positive is a backwards rotatoin. 1 and -1 are full throttle in either direction
-        #sneezy.angle = start+20 + x*(incMove - .30)#the .3 adjustment here makes him lunge forward like he is sneezing / .4 for smooth operation
-        #bashful.angle = stop-30 - x*(incMove - .40)
-        #snow.angle = start + x*incMove
-        time.sleep(incTime)
-    #grumpy.throttle = 0 #This will br used if we want to change the direction of rotation.
-    for x in range(100):
-        witch.angle = stop - x*incMove
-        #doc.angle = start-20 + x*(incMove + .3)
-        #happy.angle = stop-20 - x*(incMove - .35)
-        #dopey.angle = stop-15 - x*(incMove - .35)
-        #sleepy.angle = stop-30 - x*(incMove - .35)
-        #grumpy.throttle = .125  This will be used if we want to change the direction of rotation.
-        #sneezy.angle = stop-30 - x*(incMove - .40)
-        #bashful.angle = start+10 + x*(incMove - .40)
-        #snow.angle = stop - x*incMove
-        time.sleep(incTime)
-        #king (53,130,4)
-         #queen linear (33,120,12)
- '''       
+  
         
 class Actor(object):
     #initializer
