792907e3d2e55ffd9ed3019dd5e919e17dcec344
Todd Kleman <toddkleman@gmail.com>
2023-03-14 16:00:21 -0500

    Add files via upload

--- CuckooServo.py
+++ CuckooServo.py
@@ -18,9 +18,33 @@ clock = servo.Servo(pca.channels[3]) #30,150,2
 
 
 def moveServo(name,start: int,stop:int,delta:int):        
-        incMove = (stop-start)/100.0
-        incTime = delta/100.0
-        #using start angle(first value in moveServo) plus incremental moves(incMove) to rotate servo to stop angle in time(delta) increments(incTime) specified
+    incMove = (stop-start)/100.0
+    incTime = delta/100.0
+    #using start angle(first value in moveServo) plus incremental moves(incMove) to rotate servo to stop angle in time(delta) increments(incTime) specified
+    for x in range(100):
+        name.angle = start + x*incMove         
+        time.sleep(incTime)
+    for x in range(100):
+        name.angle = stop - x*incMove        
+        time.sleep(incTime)
+
+def moveServo_2(start: int,stop:int,delta:int):        
+    incMove = (stop-start)/100.0
+    incTime = delta/100.0
+    #using start angle(first value in moveServo) plus incremental moves(incMove) to rotate servo to stop angle in time(delta) increments(incTime) specified
+    for x in range(100):
+        king.angle = (start+13) + x*incMove
+        queen.angle = start + x*incMove
+        time.sleep(incTime) 
+    for x in range(100):
+        king.angle = stop - x*incMove-.13
+        queen.angle = stop - x*incMove                
+        time.sleep(incTime)
+def moveServoInf(name,start:int,stop:int,delta:int):        
+    incMove = (stop-start)/100.0
+    incTime = delta/100.0
+    #using start angle(first value in moveServo) plus incremental moves(incMove) to rotate servo to stop angle in time(delta) increments(incTime) specified
+    while True:
         for x in range(100):
             name.angle = start + x*incMove         
             time.sleep(incTime)
@@ -28,41 +52,29 @@ def moveServo(name,start: int,stop:int,delta:int):
             name.angle = stop - x*incMove        
             time.sleep(incTime)
 
-def moveServo_2(start: int,stop:int,delta:int):        
-        incMove = (stop-start)/100.0
-        incTime = delta/100.0
-        #using start angle(first value in moveServo) plus incremental moves(incMove) to rotate servo to stop angle in time(delta) increments(incTime) specified
-        for x in range(100):
-            if king.angle <=120:
-                king.angle = (start+13) + 1.75*x*incMove
-                queen.angle = start + x*incMove                
-            else:
-                queen.angle = start + x*incMove
-            time.sleep(incTime) 
-        for x in range(100):
-            if king.angle > 45:
-                king.angle = stop - 1.75*x*incMove
-                queen.angle = stop - x*incMove                
-            else:
-                queen.angle = stop - x*incMove
-            time.sleep(incTime)
-#cuckoo, king, queen, cuckoo, king and queen, repeat
-
-
-if __name__ == "__main__":
-    p = Process(target =moveServo, args=(clock,30,150,2,) )
-    p.start()
-    playing = True
-    while playing:
-        moveServo(cuckoo,35,115,12)
-        time.sleep(5)
+def startTheShow():
+    while True:
+        
+        moveServo(cuckoo,35,115,9)
+        time.sleep(3)
         moveServo(king,45,120,9)
-        time.sleep(5)
-        moveServo(queen,32,120,12)
-        time.sleep(5)
-        moveServo(cuckoo,35,115,12)
-        time.sleep(5)
-        moveServo_2(32,120,12)
-
+        time.sleep(3)
+        moveServo(queen,32,120,9)
+        time.sleep(6)# pause after first program
+        moveServo(cuckoo,35,115,9)
+        time.sleep(3)        
+        moveServo_2(32,120,9)
+        time.sleep(6) # pause after second program
+#cuckoo, king, queen, cuckoo, king and queen, repeat
+if __name__ == "__main__":
+        
+    p1 = Process(target=moveServoInf, args=(clock,30,150,2))
+    p1.start()
+    p2 = Process(target=startTheShow)
+    p2.start()    
+    
+    p1.join()
+    p2.join()
+    
     pca.deinit()
     pca.reset()
