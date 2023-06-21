6e571cc027efa1fc0049fa7d5cb6767e2ce2a016
Todd Kleman <toddkleman@gmail.com>
2023-06-21 09:04:24 -0500

    Add files via upload

--- CuckooServo.py
+++ CuckooServo.py
@@ -24,22 +24,25 @@ def moveServo(name,start: int,stop:int,delta:int):
     for x in range(100):
         name.angle = start + x*incMove         
         time.sleep(incTime)
+    time.sleep(5)
     for x in range(100):
         name.angle = stop - x*incMove        
         time.sleep(incTime)
-
+    time.sleep(5)
 def moveServo_2(start: int,stop:int,delta:int):        
     incMove = (stop-start)/100.0
     incTime = delta/100.0
     #using start angle(first value in moveServo) plus incremental moves(incMove) to rotate servo to stop angle in time(delta) increments(incTime) specified
     for x in range(100):
-        king.angle = (start+13) + x*incMove
+        king.angle = (start+15) + x*incMove
         queen.angle = start + x*incMove
         time.sleep(incTime) 
+    time.sleep(5)
     for x in range(100):
-        king.angle = stop - x*incMove-.13
+        king.angle = stop - x*incMove-.15
         queen.angle = stop - x*incMove                
         time.sleep(incTime)
+    time.sleep(5)
 def moveServoInf(name,start:int,stop:int,delta:int):        
     incMove = (stop-start)/100.0
     incTime = delta/100.0
@@ -55,15 +58,15 @@ def moveServoInf(name,start:int,stop:int,delta:int):
 def startTheShow():
     while True:
         
-        moveServo(cuckoo,35,115,9)
+        moveServo(cuckoo,35,110,5)
         time.sleep(3)
-        moveServo(king,45,120,9)
+        moveServo(king,45,115,5)
         time.sleep(3)
-        moveServo(queen,32,120,9)
+        moveServo(queen,30,115,5)
         time.sleep(6)# pause after first program
-        moveServo(cuckoo,35,115,9)
+        moveServo(cuckoo,35,110,5)
         time.sleep(3)        
-        moveServo_2(32,120,9)
+        moveServo_2(30,115,5)
         time.sleep(6) # pause after second program
 #cuckoo, king, queen, cuckoo, king and queen, repeat
 if __name__ == "__main__":
